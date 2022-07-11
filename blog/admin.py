c3c559dce11e5fa547c9ec53952cfe346da8e487
Erdemozer01 <20219248004@cumhuriyet.edu.tr>
2022-07-11 14:37:22 +0300

    git push -u origin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from blog.models import Contact, Titles, Cover, Inbox
+
 
 # Register your models here.
+
+@admin.register(Titles)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'created']
+
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['title', 'email', 'phone', 'address', 'created']
+
+
+@admin.register(Cover)
+class CoverAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created']
+
+
+@admin.register(Inbox)
+class InboxAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'topic', 'phone', 'created']
+    list_filter = ['name']
+
+
+
+
