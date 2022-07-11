535c9b04b8f90c10337ea32c5e25d1cd3b569319
Erdemozer01 <20219248004@cumhuriyet.edu.tr>
2022-07-11 16:17:00 +0300

    git push -u origin

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('', include('django.contrib.auth.urls')),
                   path('ckeditor/', include('ckeditor_uploader.urls')),
                   path('', include('blog.urls')),
                   path('posts/', include('posts.urls')),
